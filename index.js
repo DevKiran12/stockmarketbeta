72baf06781b554d3a03bd6f9fc7218f11797d301
DevKiran12 <kbordiwala1990@gmail.com>
2024-01-12 00:41:33 +0530

    stockmarketcode

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 
 const path = require('path');
-
+const request = require('request');
 const app = express();
 
 const Port = process.env.PORT || 5000;
+
+// API KEy :- pk_886d96de230e4da68c5765220514c4a9
+//create call API function 
+function call_api() {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_886d96de230e4da68c5765220514c4a9',{json:true},(err,res,body)=> {
+        if (err) {return console.log(err);}
+        // console.log(body);
+        if(res.statusCode===200){
+            // console.log(body);
+            return body
+        }
+    });
+}
+
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -17,8 +31,10 @@ app.set('views', './views');
 const otherstuff = "hello there this is other stuff!"
 
 app.get('/', function (req, res)  {
+    const api = call_api();
+    console.log(api);
     res.render('home',{ 
-        stuff: otherstuff
+        stock: api
 
     } );
 });
